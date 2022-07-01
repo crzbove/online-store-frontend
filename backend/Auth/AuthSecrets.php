2d5a8e4b704a36e0e439e905e2432275704aa6be
Vitaly <crzbove@gmail.com>
2022-07-01 16:33:50 +0300

    Delete backend directory

--- backend/Auth/AuthSecrets.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Helpers;
-
-define('PasswordsSalt', $_ENV['PasswordsSalt']);
-define('CookieSalt', $_ENV['CookieSalt']);
-define('CookieName', $_ENV['CookieName']);
-define('CookieDomain', $_ENV['CookieDomain']);
--- backend/Database/dbconfig.php
+++
@@ -1,5 +0,0 @@
-<?php
-define('host', $_ENV['db_host']);
-define('username', $_ENV['db_username']);
-define('password', $_ENV['db_password']);
-define('database', $_ENV['db_name']);
